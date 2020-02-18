ed7fb8abd6378ecbe04ef86a30d3fa6844a3ae0f
Hilario Mendez <hmendezvallejo@unomaha.edu>
2020-02-18 15:46:56 -0600

    removed todo's

--- webproject/src/MyServletMendez.java
+++ webproject/src/MyServletMendez.java
@@ -19,14 +19,12 @@ public class MyServletMendez extends HttpServlet {
      */
     public MyServletMendez() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Hello World!");
 	}
 
@@ -34,7 +32,6 @@ public class MyServletMendez extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
